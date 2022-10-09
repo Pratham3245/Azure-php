3a1f4b8c13c165f3265a88da2b6c329eac39438d
Pratham <dhamechapratham@gmail.com>
2022-10-09 14:59:52 +0530

    master

--- index.php
+++ index.php
@@ -334,7 +334,7 @@ include './php/contactus.php';
         <div class="row">
           <div class="col-lg-4 col-md-6">
             <div class="icon-box" data-aos="zoom-in-left">
-              <div class="icon"><i class="bi bi-briefcase" style="color: #ff689b;"></i></div>
+              <div class="icon"><i class="fa fa-book" style="color: #ff689b;"></i></div>
               <h4 class="title"><a href="">Lorem Ipsum</a></h4>
               <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint
                 occaecati cupiditate non provident</p>
@@ -342,7 +342,7 @@ include './php/contactus.php';
           </div>
           <div class="col-lg-4 col-md-6 mt-5 mt-md-0">
             <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="100">
-              <div class="icon"><i class="bi bi-book" style="color: #e9bf06;"></i></div>
+              <div class="icon"><i class="fa fa-book" style="color: #e9bf06;"></i></div>
               <h4 class="title"><a href="">Dolor Sitema</a></h4>
               <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
                 commodo consequat tarad limino ata</p>
@@ -351,7 +351,7 @@ include './php/contactus.php';
 
           <div class="col-lg-4 col-md-6 mt-5 mt-lg-0 ">
             <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="200">
-              <div class="icon"><i class="bi bi-card-checklist" style="color: #3fcdc7;"></i></div>
+              <div class="icon"><i class="fa fa-book" style="color: #3fcdc7;"></i></div>
               <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
               <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                 fugiat nulla pariatur</p>
@@ -359,7 +359,7 @@ include './php/contactus.php';
           </div>
           <div class="col-lg-4 col-md-6 mt-5">
             <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="300">
-              <div class="icon"><i class="bi bi-binoculars" style="color:#41cf2e;"></i></div>
+              <div class="icon"><i class="fa fa-book" style="color:#41cf2e;"></i></div>
               <h4 class="title"><a href="">Magni Dolores</a></h4>
               <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
                 mollit anim id est laborum</p>
@@ -368,7 +368,7 @@ include './php/contactus.php';
 
           <div class="col-lg-4 col-md-6 mt-5">
             <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="400">
-              <div class="icon"><i class="bi bi-globe" style="color: #d6ff22;"></i></div>
+              <div class="icon"><i class="fa fa-book" style="color: #d6ff22;"></i></div>
               <h4 class="title"><a href="">Nemo Enim</a></h4>
               <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
                 praesentium voluptatum deleniti atque</p>
@@ -376,7 +376,7 @@ include './php/contactus.php';
           </div>
           <div class="col-lg-4 col-md-6 mt-5">
             <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="500">
-              <div class="icon"><i class="bi bi-clock" style="color: #4680ff;"></i></div>
+              <div class="icon"><i class="fa fa-book" style="color: #4680ff;"></i></div>
               <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
               <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum
                 soluta nobis est eligendi</p>
@@ -494,19 +494,19 @@ include './php/contactus.php';
           <div class="col-lg-4" data-aos="fade-right">
             <div class="info">
               <div class="address">
-                <i class="bi bi-geo-alt"></i>
+                <i class="fas fa-map-marker-alt"></i>
                 <h4>Location:</h4>
                 <p>Ahmedabad , India</p>
               </div>
 
               <div class="email">
-                <i class="bi bi-envelope"></i>
+                <i class="fa fa-envelope"></i>
                 <h4>Email:</h4>
                 <p>bookopedia@gmail.com</p>
               </div>
 
               <div class="phone">
-                <i class="bi bi-phone"></i>
+                <i class="fa fa-mobile"></i>
                 <h4>Call:</h4>
                 <p>+911234567890</p>
               </div>
